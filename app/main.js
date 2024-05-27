3185fe8ce81e750c0564ab7476cd09f35daddb1e
hh <hh@hhs-iMac-Pro.local>
2024-05-27 18:17:21 +0700

    added git cat-file

--- app/main.js
+++ app/main.js
@@ -1,25 +1,49 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("node:zlib");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
- const command = process.argv[2];
+const command = process.argv[2];
 
- switch (command) {
-   case "init":
-     createGitDirectory();
-     break;
-   default:
-     throw new Error(`Unknown command ${command}`);
- }
+switch (command) {
+  case "init":
+    createGitDirectory();
+    break;
+  case "cat-file":
+    readGitBlob(process.argv[3]);
+    break;
+  default:
+    throw new Error(`Unknown command ${command}`);
+}
 
- function createGitDirectory() {
-   fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
-   fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
-   fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
+function createGitDirectory() {
+  fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
+  fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), {
+    recursive: true,
+  });
+  fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
 
-   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
-   console.log("Initialized git directory");
- }
+  fs.writeFileSync(
+    path.join(process.cwd(), ".git", "HEAD"),
+    "ref: refs/heads/main\n",
+  );
+  console.log("Initialized git directory");
+}
+
+function readGitBlob(SHA) {
+  const blobPath = path.resolve(
+    ".git",
+    "objects",
+    SHA.slice(0, 2),
+    SHA.slice(2),
+  );
+
+  let data = fs.readFileSync(blobPath);
+  let dataUncompressed = zlib.unzipSync(data);
+  let nullByteIndex = dataUncompressed.indexOf("\0");
+  console.log(dataUncompressed.toString().slice(nullByteIndex + 1));
+  return dataUncompressed.toString().slice(nullByteIndex + 1);
+}
