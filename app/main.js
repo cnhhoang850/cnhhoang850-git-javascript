a31f17972793f065f3aceb19081a41b837bb3609
hh <hh@hhs-iMac-Pro.local>
2024-05-27 18:19:35 +0700

    fixed again

--- app/main.js
+++ app/main.js
@@ -3,8 +3,6 @@ const path = require("path");
 const zlib = require("node:zlib");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
 
