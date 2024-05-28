3e52666d9e8362d958020b75574c8111a07ffbcf
hh <hh@hhs-iMac-Pro.local>
2024-05-28 22:20:20 +0700

    ls-tree

--- app/main.js
+++ app/main.js
@@ -58,28 +58,32 @@ switch (command) {
 }
 
 function lsTree() {
-  const flag = process.argv[3];
-  if (flag == "--name-only") {
-    const sha = process.argv[4];
-    const directory = sha.slice(0, 2);
-    const fileName = sha.slice(2);
-    const filePath = path.join(
-      __dirname,
-      ".git",
-      "objects",
-      directory,
-      fileName,
-    );
-    let inflatedContent = zlib
-      .inflateSync(fs.readFileSync(filePath))
-      .toString()
-      .split("\0");
-    let content = inflatedContent
-      .slice(1)
-      .filter((value) => value.includes(" "));
-    let names = content.map((value) => value.split(" ")[1]);
-    names.forEach((name) => process.stdout.write(`${name}\n`));
+  const isNameOnly = process.argv[3];
+  let hash = "";
+  if (isNameOnly === "--name-only") {
+    //display the name only
+    hash = process.argv[4];
+  } else {
+    hash = process.argv[3];
   }
+  const dirName = hash.slice(0, 2);
+  const fileName = hash.slice(2);
+  const objectPath = path.join(".git", "objects", dirName, fileName);
+  const dataFromFile = fs.readFileSync(objectPath);
+  //decrypt the data from the file
+  const inflated = zlib.inflateSync(dataFromFile);
+  //notice before encrypting the data what we do was we encrypt
+  //blob length/x00 so to get the previous string back what we need to do is split with /xoo
+  const enteries = inflated.toString("utf-8").split("\x00");
+  //enteries will be [blob length/x00, actual_file_content]
+  const dataFromTree = enteries.slice(1);
+  const names = dataFromTree
+    .filter((line) => line.includes(" "))
+    .map((line) => line.split(" ")[1]);
+  const namesString = names.join("\n");
+  const response = namesString.concat("\n");
+  //this is the regex pattern that tells to replace multiple global \n with single \n
+  process.stdout.write(response.replace(/\n\n/g, "\n"));
 }
 
 function createGitDirectory() {
@@ -138,8 +142,5 @@ function readGitBlob(sha) {
   let data = fs.readFileSync(blobPath);
   let dataUncompressed = zlib.unzipSync(data);
   let nullByteIndex = dataUncompressed.indexOf("\0");
-  return dataUncompressed
-    .toString()
-    .slice(nullByteIndex + 1)
-    .replace(/(\r\n|\n|\r)/gm, "");
+  return dataUncompressed.toString().slice(nullByteIndex + 1);
 }
