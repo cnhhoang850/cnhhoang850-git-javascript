e8ba6a0d6565a0f4ec10f358a19ec2a437be58f7
Cao Hoang <cnhhoang850@gmail.com>
2024-06-13 10:30:06 +0700

    done

--- app/modules/writeFolderAsTree.js
+++ app/modules/writeFolderAsTree.js
@@ -24,7 +24,7 @@ function writeFolderAsTree(basePath = "") {
       entries.push({
         mode: 40000,
         name: file,
-        hash: writeFolderAsTree(path.join(gitDirectory, fullPath)),
+        hash: writeFolderAsTree(fullPath),
       });
     }
   }
