24cc3896f7320572e0937d949fc826e6f8485f33
hh <hh@hhs-iMac-Pro.local>
2024-06-13 18:22:50 +0700

    done

--- app/modules/writeBlob.js
+++ app/modules/writeBlob.js
@@ -1,31 +1,31 @@
-const fs = require('fs')
-const path = require('path')
-const { sha1, writeGitObject } = require('./utils')
+const fs = require("fs");
+const path = require("path");
+const { sha1, writeGitObject } = require("./utils");
 
-function hashBlob (write, fileName, basePath = '') {
+function hashBlob(write, fileName, basePath = "") {
   // Read file
-  const filePath = path.resolve(basePath, fileName)
+  const filePath = path.resolve(basePath, fileName);
 
   // Write git blob
-  let data = fs.readFileSync(filePath).toString()
+  let data = fs.readFileSync(filePath).toString();
 
   // Add header
-  data = `blob ${data.length}\0` + data
-  const writeFilePath = sha1(data)
+  data = `blob ${data.length}\0` + data;
+  const writeFilePath = sha1(data);
 
   // Write to directory
-  let hash
+  let hash;
   if (write) {
-    hash = writeGitObject(writeFilePath, data, basePath)
+    hash = writeGitObject(writeFilePath, data, basePath);
   }
 
   // Log and return hash
   if (hash) {
-    process.stdout.write(hash + '\n') // Append newline here
-    return hash
+    //process.stdout.write(hash + '\n') // Append newline here
+    return hash;
   } else {
-    throw new Error('No hash')
+    throw new Error("No hash");
   }
 }
 
-module.exports = hashBlob
+module.exports = hashBlob;
